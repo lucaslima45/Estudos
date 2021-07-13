c60ce7bceebe47d26ae4910931dd844ca53a6d91
Lucas Henrique Lima <datassac@gmail.com>
2021-07-13 15:32:56 -0300

    Add files via upload

---
+++ exercicioif.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+idade = int(input('Qual a sua idade?'))
+if idade >= 18:
+    print('Você é adulto.')
+else:
+    print('Você ainda não é adulto.')
+
+
+# In[2]:
+
+
+#outra forma de fazer esse exercício é com o ano de nascimento da pessoa
+
+
+# In[3]:
+
+
+ano = int(input('Em que ano você nasceu?'))-2021
+if ano>=18:
+    print('Você é adulto.')
+else:
+    print('Você ainda não é adulto.')
+
+
+# In[ ]:
+
+
+
+
