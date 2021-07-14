6dc48469d5948d007f18d594af06d71906198bbf
Lucas Henrique Lima <datassac@gmail.com>
2021-07-14 17:59:56 -0300

    Add files via upload

---
+++ exerciciosorteio.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import random
+cidades = ['São paulo','Rio de Janeiro', 'Belo Horizonte','Porto Alegre']
+escolhida = random.choice(cidades)
+print('Parabéns! a cidade {0} foi escolhida para a abertura da copa!' .format(escolhida))
+
+
+# In[ ]:
+
+
+
+
